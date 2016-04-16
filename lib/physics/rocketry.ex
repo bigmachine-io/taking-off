d3af1f01ce5968cc568b32d8cc6e4f9ddc1227b4
Rob Conery <rob@conery.io>
2016-04-16 16:13:25 -0700

    Added Mars and Moon Ev Calcs

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -67,6 +67,16 @@ end
 
 defmodule Physics.Rocketry do
 
+  def escape_velocity(:moon) do
+    %{mass: 7.35e22, radius: 1.738e6}
+      |> escape_velocity
+  end
+
+  def escape_velocity(:mars) do
+    %{mass: 6.39e23, radius: 3.4e6}
+      |> escape_velocity
+  end
+
   def escape_velocity(:earth) do
     %{mass: 5.972e24, radius: 6.371e6}
       |> escape_velocity
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -1,17 +1,44 @@
 defmodule PhysicsTest do
   use ExUnit.Case
-  doctest Physics
+
+  test "escape velocity of earth is correct" do
+    ev = Physics.Rocketry.escape_velocity(:earth)
+    assert ev == 11.2 #what should this value be?
+  end
+
+  test "escape velocity of mars is correct" do
+    ev = Physics.Rocketry.escape_velocity(:mars)
+    assert ev == 5.0 #what should this value be?
+  end
+
+  test "escape velocity of moon is correct" do
+    ev = Physics.Rocketry.escape_velocity(:moon)
+    assert ev == 2.4 #what should this value be?
+  end
+
+  test "escape velocity of planet X is correct" do
+    ev =  %{mass: 4.0e22, radius: 6.21e6} |> Physics.Rocketry.escape_velocity
+    assert ev == 0.9
+    #use assert here
+  end
+
+  test "Converting to to_light_seconds" do
+    ls = Converter.to_light_seconds({:miles, 1000}, precision: 5)
+    assert ls == 0.00537
+  end
 
   test "Converter one works" do
-    Converter.to_light_seconds({:miles, 1000}, precision: 5) |> IO.inspect 
-    #assert ... what exactly?
+    ls = Converter.to_light_seconds({:miles, 1000}, precision: 5)
+    assert ls == 0.00537
   end
 
   test "ConverterTwo works" do
-    ConverterTwo.to_light_seconds({:miles, 1000}, precision: 5) |> IO.inspect 
+    ls = ConverterTwo.to_light_seconds({:miles, 1000}, precision: 5)
+    assert ls == 0.00537
   end
 
   test "ConverterTwo works with default values" do
-    ConverterTwo.to_light_seconds({:miles, 1000}) |> IO.inspect 
+    ls = ConverterTwo.to_light_seconds({:miles, 1000})
+    assert ls == 0.00537
   end
 end
