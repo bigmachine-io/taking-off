8d9db7cadcb43efc75b431d325e1dc5d50161787
Rob Conery <rob@conery.io>
2016-04-16 15:56:00 -0700

    Added ConverterTwo with a case statement

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -30,6 +30,27 @@ defmodule Converter do
   def round_up(val) when is_float(val), do: trunc(val)
 end
 
+defmodule ConverterTwo do
+  
+  #...
+
+  def to_light_seconds({unit, val}, precision: precision) do 
+    case unit do 
+      :miles -> from_miles(val)
+      :meters -> from_meters(val)
+      :feet -> from_feet(val)
+      :inches -> from_inches(val)
+    end |> round_to(precision) # example 1
+  end
+
+  defp from_miles(val), do: val * 5.36819e-6
+  defp from_meters(val), do: val * 3.335638620368e-9
+  defp from_feet(val), do: val * 1.016702651488166404e-9
+  defp from_inches(val), do: val * 8.472522095734715723e-11
+  defp round_to(val, precision), do: Float.round(val, precision)
+  
+end
+
 defmodule Physics.Rocketry do
 
   def escape_velocity(:earth) do
