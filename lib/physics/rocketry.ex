e04a7d0ba6d486c6ada47cf61f3583c5f0619008
Rob Conery <rob@conery.io>
2016-04-16 16:00:03 -0700

    Added some tests for our converters

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,6 +1,39 @@
+defmodule ConverterTwo do
+  
+  def to_nearest_tenth(val) do
+    round_to(val, 1)
+  end
+
+  def to_km(val) do
+    val / 1000
+  end
+
+  def to_meters(val) do
+    val * 1000
+  end
+  
+  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
+  def to_light_seconds({unit, val}, precision: precision) do 
+    light_seconds = case unit do 
+      :miles -> from_miles(val)
+      :meters -> from_meters(val)
+      :feet -> from_feet(val)
+      :inches -> from_inches(val)
+    end
+    light_seconds |> round_to(precision) # example 2
+  end
+
+  defp from_miles(val), do: val * 5.36819e-6
+  defp from_meters(val), do: val * 3.335638620368e-9
+  defp from_feet(val), do: val * 1.016702651488166404e-9
+  defp from_inches(val), do: val * 8.472522095734715723e-11
+  defp round_to(val, precision), do: Float.round(val, precision)
+  
+end
+
 defmodule Converter do
   def to_nearest_tenth(val) do
-    Float.ceil(val, 1)
+    round_to(val, 1)
   end
 
   def to_km(val) do
@@ -11,44 +44,25 @@ defmodule Converter do
     val * 1000
   end
 
-  def to_light_seconds({:miles, miles} = val) do
-    (miles * 5.36819e-6) |> round_up
+  def to_light_seconds({:miles, miles} = val, precision: precision) do
+    (miles * 5.36819e-6) |> round_to(precision)
   end
 
-  def to_light_seconds({:meters, meters} = val)  do
-    (meters * 3.335638620368e-9) |> round_up
+  def to_light_seconds({:meters, meters} = val, precision: precision)  do
+    (meters * 3.335638620368e-9) |> round_to(precision)
   end
 
-  def to_light_seconds({:feet, feet} = val)  do
-    (feet * 1.016702651488166404e-9) |> round_up
+  def to_light_seconds({:feet, feet} = val, precision: precision)  do
+    (feet * 1.016702651488166404e-9) |> round_to(precision)
   end
 
-  def to_light_seconds({:inches, inches} = val)  do
-    (inches * 8.472522095734715723e-11) |> round_up
+  def to_light_seconds({:inches, inches} = val, precision: precision)  do
+    (inches * 8.472522095734715723e-11) |> round_to(precision)
   end
 
-  def round_up(val) when is_float(val), do: trunc(val)
-end
-
-defmodule ConverterTwo do
-  
-  #...
-
-  def to_light_seconds({unit, val}, precision: precision) do 
-    case unit do 
-      :miles -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet -> from_feet(val)
-      :inches -> from_inches(val)
-    end |> round_to(precision) # example 1
+  defp round_to(val, precision) when is_float(val) do
+    Float.round(val, precision)
   end
-
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-  defp round_to(val, precision), do: Float.round(val, precision)
-  
 end
 
 defmodule Physics.Rocketry do
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -2,14 +2,16 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "escape velocity of earth is correct" do
-    #notice how you can pipe directly to IO.inspect? And still get the value returned...
-    ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
-    assert ev == 0 #what should this value be?
+  test "Converter one works" do
+    Converter.to_light_seconds({:miles, 1000}, precision: 5) |> IO.inspect 
+    #assert ... what exactly?
   end
-  test "escape velocity of planet X is correct" do
-    planet_x = %{mass: 4.0e22, radius: 6.21e6}
-    ev =  planet_x |> Physics.Rocketry.escape_velocity |> IO.inspect
-    #use assert here
+
+  test "ConverterTwo works" do
+    ConverterTwo.to_light_seconds({:miles, 1000}, precision: 5) |> IO.inspect 
+  end
+
+  test "ConverterTwo works with default values" do
+    ConverterTwo.to_light_seconds({:miles, 1000}) |> IO.inspect 
   end
 end
