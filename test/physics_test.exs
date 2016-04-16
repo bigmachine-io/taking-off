06bbbd2cd77045a1fd1d17b36128557a834c672c
Rob Conery <rob@conery.io>
2016-04-16 15:21:53 -0700

    Added Converter and some tests

--- test/physics_test.exs
+++ test/physics_test.exs
@@ -2,7 +2,14 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "escape velocity of earth is correct" do
+    #notice how you can pipe directly to IO.inspect? And still get the value returned...
+    ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
+    assert ev == 0 #what should this value be?
+  end
+  test "escape velocity of planet X is correct" do
+    planet_x = %{mass: 4.0e22, radius: 6.21e6}
+    ev =  planet_x |> Physics.Rocketry.escape_velocity |> IO.inspect
+    #use assert here
   end
 end
